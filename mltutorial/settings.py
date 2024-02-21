d8dbb197c5657bd7f395695a9ac3fb18163f4bc3
nzh2534 <91237859+nzh2534@users.noreply.github.com>
2024-02-21 14:17:25 -0700

    initial commit

--- mltutorial/settings.py
+++ mltutorial/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-vq0b@2vozsh%y85l#b7+cp58=_s#cutvymb9ckr_e*9_qd48dp"
+SECRET_KEY = "" #Generate and Add your Django Secret Key here
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
